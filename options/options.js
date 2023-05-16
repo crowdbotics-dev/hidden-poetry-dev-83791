53182c354cccb5de4ec58d3b7bc3e2bfbf679d3f
Crowdbotics <engineering@crowdbotics.com>
2023-05-16 12:05:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hidden-poetry-dev-83791",
+  url: "https://hidden-poetry-dev-83791.botics.co",
+  api: "https://hidden-poetry-dev-83791.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
